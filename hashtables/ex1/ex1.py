867b5968ec247386f3767a5032425e9a8fa83526
ola-dola <oredolaolamide@gmail.com>
2020-07-10 09:16:13 +0100

    ex1.py solution

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,14 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    for i, val in enumerate(weights):
+        if limit-val in cache:
+            return (i, cache[limit-val])
+        else:
+            cache[val] = i
 
     return None
+
+
+print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
