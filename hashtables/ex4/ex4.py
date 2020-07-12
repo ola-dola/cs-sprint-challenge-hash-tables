ff31b4678cc28709b97cff43ade4631139bc93e9
ola-dola <oredolaolamide@gmail.com>
2020-07-12 19:36:32 +0100

    ex4 solution

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,16 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    cache = {}
+
+    for num in a:
+        if num > 0:
+            cache[num] = num
+
+    for num in a:
+        if num < 0 and abs(num) in cache:
+            result.append(abs(num))
 
     return result
 
